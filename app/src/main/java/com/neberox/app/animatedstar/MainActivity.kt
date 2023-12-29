4c178f8b6870dc48cf6fb066909e02169fc2af06
Muhammad Umar <ee_umar@yahoo.com>
2023-12-29 20:11:09 +0000

    added listener on rating change

--- app/src/main/java/com/neberox/app/animatedstar/MainActivity.kt
+++ app/src/main/java/com/neberox/app/animatedstar/MainActivity.kt
@@ -1,46 +1,17 @@
 package com.neberox.app.animatedstar
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.CheckBox
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.neberox.app.animatedstar.ui.theme.AnimatedStarRatingTheme
+import com.neberox.library.animatedstar.StarRatingBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AnimatedStarRatingTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.rating)
+        findViewById<StarRatingBar>(R.id.star_rating).setOnRatingChangeListener { rating ->
+            Log.d("Rating", rating.toString())
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AnimatedStarRatingTheme {
-        Greeting("Android")
     }
 }
